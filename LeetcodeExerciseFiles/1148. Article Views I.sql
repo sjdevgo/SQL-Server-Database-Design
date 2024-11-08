af077abfc63c0e31664dfdf418cc1f0b73aa4678
sjiang20 <sjiang086@gmail.com>
2024-11-07 23:18:55 -0800

    595/1148

---
+++ LeetcodeExerciseFiles/1148. Article Views I.sql
@@ -0,0 +1,12 @@
+-- There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+-- Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+-- Note that equal author_id and viewer_id indicate the same person.
+ 
+
+-- Write a solution to find all the authors that viewed at least one of their own articles.
+
+-- Return the result table sorted by id in ascending order.
+
+select DISTINCT author_id as id from Views
+where author_id = viewer_id
+order by id
