6c4621aa92859f3c73a17f6c61d9fb1b0cb8bd07
sjiang20 <sjiang086@gmail.com>
2025-04-24 17:46:45 -0700

    1757

---
+++ SQL/1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,7 @@
+-- Product table with Product_id, low_fats, recyclable
+-- Write a solution to find the ids of products that are both low fat and recyclable.
+-- Return the result table in any order.
+
+select product_id 
+from products
+where low_fats = 'Y' and recyclable = 'Y'
